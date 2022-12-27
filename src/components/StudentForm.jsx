787e8f14bad2a95fad75473687eefeb4e1bc5cd2
puneethkumar-v <pk.puni872@gmail.com>
2022-12-27 22:15:26 +0530

    Updated components with props and state management

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -1,54 +1,218 @@
-import React, { useState } from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { Formik } from "formik";
 
-const StudentForm = () => {
-  const [state, setState] = useState([]);
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [rollNo, setRollNo] = useState("");
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(name, phone, rollNo);
-  }
+import React from "react";
+import { useState } from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import StudentInfo from "./StudentInfo";
+// import CircularProgress from "@material-ui/core/CircularProgress";
+
+const StudentForm = ({
+  handleFormSubmit,
+  initialValues,
+  checkoutSchema,
+  arrayOfStudents,
+  setArrayOfStudents,
+}) => {
+  const isNonMobile = useMediaQuery("(min-width:650px)");
+
+  const [loading, setLoading] = useState(false);
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <div className="from-group">
-        <label htmlFor="roll-no">Roll No</label>
-        <input
-          id="roll-no"
-          type="number"
-          value={rollNo}
-          onChange={(e) => setRollNo(e.target.value)}
-        />
-      </div>
-      <div className="from-group">
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div className="from-group">
-        <label htmlFor="phone">Phone No</label>
-        <input
-          id="phone"
-          type="tel"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-        />
-      </div>
-      {/* <div className="from-group">
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
-      </div>
-      <div className="from-group">
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
-      </div> */}
-      <input type="submit" value="Save" id="submit-btn" />
-    </form>
+    <Box m="20px" sx={{ height: isNonMobile ? "90vh" : "100%" }}>
+      <Box
+        m="20px"
+        sx={{
+          height: isNonMobile ? "80vh" : "100%",
+        }}
+      >
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={checkoutSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+          }) => (
+            <form
+              onSubmit={handleSubmit}
+              style={{
+                width: "100%",
+                boxShadow: isNonMobile
+                  ? "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)"
+                  : null,
+                maxWidth: "40rem",
+                margin: "0 auto",
+                padding: isNonMobile ? "2rem" : null,
+                borderRadius: "6px",
+              }}
+            >
+              <Typography variant={isNonMobile ? "h3" : "h4"} mb="2rem">
+                REGISTRATION FORM
+              </Typography>
+              <Box
+                display="grid"
+                gap="30px"
+                gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                sx={{
+                  "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                }}
+              >
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="email"
+                  label="Email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email}
+                  name="email"
+                  error={!!touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  name="firstName"
+                  error={!!touched.firstName && !!errors.firstName}
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  name="lastName"
+                  error={!!touched.lastName && !!errors.lastName}
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Country"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.country}
+                  name="country"
+                  error={!!touched.country && !!errors.country}
+                  helperText={touched.country && errors.country}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="State"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.state}
+                  name="state"
+                  error={!!touched.state && !!errors.state}
+                  helperText={touched.state && errors.state}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="City"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.city}
+                  name="city"
+                  error={!!touched.city && !!errors.city}
+                  helperText={touched.city && errors.city}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Zip"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.zip}
+                  name="zip"
+                  error={!!touched.zip && !!errors.zip}
+                  helperText={touched.zip && errors.zip}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="textarea"
+                  label="Address"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.address}
+                  name="address"
+                  error={!!touched.address && !!errors.address}
+                  helperText={touched.address && errors.address}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="tel"
+                  label="contact_number"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.contact_number}
+                  name="contact_number"
+                  error={!!touched.contact_number && !!errors.contact_number}
+                  helperText={touched.contact_number && errors.contact_number}
+                  sx={{ gridColumn: "span 4" }}
+                />
+              </Box>
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                mt="20px"
+              >
+                {loading ? (
+                  // <CircularProgress />
+                  "loading"
+                ) : (
+                  <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    sx={{
+                      padding: isNonMobile ? "10px 20px" : null,
+                      width: "100%",
+                      fontSize: isNonMobile ? "16px" : null,
+                      letterSpacing: "0.15rem",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    ADD STUDENT
+                  </Button>
+                )}
+              </Box>
+            </form>
+          )}
+        </Formik>
+      </Box>
+    </Box>
   );
 };
 
--- src/components/StudentInfo.jsx
+++ src/components/StudentInfo.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-const StudentInfo = () => {
+export default function StudentInfo({ rows }) {
+  let count = 1;
   return (
-    <div>StudentInfo</div>
-  )
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>SL No</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">First name</TableCell>
+            <TableCell align="center">Last Name</TableCell>
+            <TableCell align="center">Country</TableCell>
+            <TableCell align="center">State</TableCell>
+            <TableCell align="center">City</TableCell>
+            <TableCell align="center">Zip</TableCell>
+            <TableCell align="center">Address</TableCell>
+            <TableCell align="center">Phone</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, id) => (
+            <TableRow
+              key={id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {id + 1}
+              </TableCell>
+              <TableCell align="center">{row.email}</TableCell>
+              <TableCell align="center">{row.firstName}</TableCell>
+              <TableCell align="center">{row.lastName}</TableCell>
+              <TableCell align="center">{row.country}</TableCell>
+              <TableCell align="center">{row.state}</TableCell>
+              <TableCell align="center">{row.city}</TableCell>
+              <TableCell align="center">{row.zip}</TableCell>
+              <TableCell align="center">{row.address}</TableCell>
+              <TableCell align="center">{row.contact_number}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
-
-export default StudentInfo
